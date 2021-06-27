f90885ad9b591b7c6467d7e6dc953ca5334dbf0e
Andre Quispesaravia <andreq@anjis-mbp.lan>
2021-06-27 00:03:18 -0700

    [Nit] add title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Avocardo</title>
           <link rel="shortcut icon" href="/img/favicon.ico" />
         </Head>
         <body
