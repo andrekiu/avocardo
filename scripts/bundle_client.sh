0977efbc3a34281d78a0a9e698f43b2f4ccd939f
Andre Quispesaravia <andreq@anjis-mbp.lan>
2021-06-27 00:15:21 -0700

    [Extension] add favicon

--- scripts/bundle_client.sh
+++ scripts/bundle_client.sh
@@ -2,6 +2,7 @@ REMOVE_LAST=$(rm build/chrome_extension/*)
 SRC="
   src/chrome_extension/config/background.js
   src/chrome_extension/config/script.js
+  public/img/favicon.png
 "
 CAT_MANIFEST=$(cat src/chrome_extension/config/manifest.json.$1 > build/chrome_extension/manifest.json)
 CAT_INDEX=$(cat src/chrome_extension/config/index.html.$1 > build/chrome_extension/index.html)
